25f8f9626b186caa78e635c6018f872cf961bbdf
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-10-18 18:50:45 -0600

    adds new routes/methods

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,12 @@ class ActorsController < ApplicationController
     actor_2 = Actor.find_by(id: 2)
     render json: actor_2
   end
+  def index
+    actors = Actor.all
+    render json: actors.as_json
+  end
+  def show
+    actor = Actor.find_by(id: params[:id])
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/actor_2" => "actors#actor_id_2"
   get "/all_movies" => "movies#all_movies"
   get "/random_movie" => "movies#random_movie"
+  get "actors" => "actors#index"
+  get "actors/:id" => "actors#show"
 end
